9e1889d55ecf91f2b144e29c8dc29149d9e4d457
ddiieeggoo b <zkpgmi@gmail.com>
2023-12-04 16:39:58 +0100

    pulling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def show
+    if user_signed_in? && current_user.transaction.state == 'pending'
+      @transactions = Transaction.all
+    end
+  end
+
+  def validate
+    @transaction = Transaction.find(params[:id])
+    @transaction.state = 'validate'
+    redirect_to profile_path(@transaction.nft_id.user_id)
+  end
+
+  def refuse
+    @transaction = Transaction.find(params[:id])
+    @transaction.destroy
+    redirect_to profile_path(@transaction.nft_id.user_id)
+  end
 end
