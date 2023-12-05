e9a5917fef25b2510bfe43b1d90df594146a058d
Nina Laouadi <nina.laouadi0@gmail.com>
2023-12-05 18:44:57 +0100

    reservation button

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,20 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"
+// import rangePlugin from "flatpickrRangePlugin";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
-  connect() {
+  static targets = [ "begin_at", "end_at" ]
+  // static values = {
+  //       disabledStartDate: Array
+  //       }
 
-    flatpickr("#dateRangePicker", {
+
+  connect() {
+    // console.log(this.disabledStartDateValue)
+    flatpickr(this.begin_atTarget, {
       mode: "range",
-      dateFormat: "Y-m-d",
-      minDate: "today",
-      disable: [
-        function(date) {
-          // disable every multiple of 8
-          return !(date.getDate() % 8);
-        }
-      ]
-    });
+      dateFormat: "d-m-Y",
+      minDate: "today"
+      // disable: this.disabledStartDateValue
+    })
+  }
+
+
+  send(){
+    const startDate = this.begin_atTarget.value.split(" to ")[0]
+    const endDate = this.begin_atTarget.value.split(" to ")[1]
+    this.end_atTarget.value = endDate
+    this.begin_atTarget.value = startDate
   }
 }
