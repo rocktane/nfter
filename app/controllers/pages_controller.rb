4f3cdf2f01322048cc063ca4c24a02042a362f25
Yohan Gouiran <y.gouiran@gmail.com>
2023-12-06 22:19:59 +0100

    add colonne rented + début code dans controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
     if user_signed_in?
     @nfts = Nft.where(user_id: current_user)
     @transactions = Transaction.where(state: 'pending', nft: @nfts)
+    @current_transactions = Transaction.where(user_id: current_user)
+    @renteds = Nft.where(transactions: @current_transactions)
     else
       redirect_to new_user_session_path
     end
