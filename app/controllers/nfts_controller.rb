c66c9dbe97580e3c90a83119905728f64ca67f16
ddiieeggoo b <zkpgmi@gmail.com>
2023-12-06 16:28:12 +0100

    saut de ligne en plus

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -7,7 +7,8 @@ class NftsController < ApplicationController
   def show
     @nft = Nft.find(params[:id])
     @transaction = Transaction.new
-    @disabled_dates = @nft.transactions.flat_map { |transaction| (transaction.begin_date.to_date..transaction.end_date.to_date).map { |date| date.strftime('%Y-%m-%d') }}
+    @disabled_dates = @nft.transactions.flat_map { |transaction| (transaction.begin_date.to_date..transaction.end_date.to_date).map { |date| date.strftime('%Y-%m-%d')
+    }}
   end
 
   def new
